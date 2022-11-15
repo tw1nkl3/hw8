96a785b3ea2fc2c639c6d4554070a70f402b19f8
Ivan <grabovivan@gmailcom>
2022-11-15 18:03:33 +0300

    num5 added

---
+++ num5/Program.cs
@@ -0,0 +1,37 @@
+﻿int a = 4;
+int[,] array = new int[a, a];
+fillArray(array, a);
+printArray(array);
+
+void fillArray(int[,] arr, int n)
+{
+    int i = 0, j = 0;
+    int value = 1;
+    for (int e = 0; e < n * n; e++)
+    {
+        int k = 0;
+        do { array[i, j++] = value++; } while (++k < n - 1);
+        for (k = 0; k < n - 1; k++) arr[i++, j] = value++;
+        for (k = 0; k < n - 1; k++) arr[i, j--] = value++;
+        for (k = 0; k < n - 1; k++) arr[i--, j] = value++;
+        ++i; ++j;
+        n = n < 2 ? 0 : n - 2;
+    }
+}
+
+void printArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (array[i, j] < 10)
+            {
+                Console.Write("0" + array[i, j]);
+                Console.Write(" ");
+            }
+            else Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
